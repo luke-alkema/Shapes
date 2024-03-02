20222db22e44190142026935b0563d2516ff8903
Dylan Shouldice-Jacobs <shouldicedylan04@gmail.com>
2024-03-02 15:34:19 -0500

    Started creating skeleton

--- Circle.h
+++ Circle.h
@@ -1,1 +1,25 @@
 #pragma once
+
+/*
+*	Class   : Circle
+*	Purpose : 
+*
+* 
+* 
+* 
+*/
+
+class Shape::Circle
+{
+	private: 
+
+		float radius; // Holds the radius of the circle, acceptable values are anything > 0.00
+
+	public:
+
+		// Constructor 
+		Circle(String colour, float radius);
+
+		//
+
+};
