cf5f5ebf4c1dab257f5490bf19ca56949964dd7a
Dylan Shouldice-Jacobs <shouldicedylan04@gmail.com>
2024-03-02 17:04:49 -0500

    Finished skeleton for Circle.cpp and create class definition in Circle.h

--- Circle.h
+++ Circle.h
@@ -1,25 +1,40 @@
 #pragma once
-
+#include "Shape.h"
 /*
 *	Class   : Circle
-*	Purpose : 
-*
-* 
-* 
-* 
+*	Purpose : The purpose of this class is to further model the shape of a circle, it is a sub-class that stems from the master-class "Shape".
+*			  The master-class defines the name and colour of the shape while circle has a radius, a defining factor of a circle which allows to obtain
+*			  Values of the circle that might be useful to us such as the circumfrence, perimeter etc.
 */
 
-class Shape::Circle
+#define PI 3.1415926
+
+class Circle : public Shape
 {
 	private: 
 
-		float radius; // Holds the radius of the circle, acceptable values are anything > 0.00
+		double radius; // Holds the radius of the circle, acceptable values are anything > 0.00
 
 	public:
 
-		// Constructor 
-		Circle(String colour, float radius);
+		// Constructors
+		
+		Circle(string colour, double radius); // Creates a circle object when the colour and radius are present, validates that radius > 0.00
+		Circle();							  // Default constructor sets the value of radius == 0.00
 
-		//
+		// Decontructors 
+
+		virtual ~Circle(); // Deletes the circle object
+
+		// Methods
+
+		double GetRadius(void);			  // Gets the current value of radius
+		bool SetRadius(double newRadius); // Sets the current value of radius and returns true if "newRadius" is > 0.00 else it will return false
+
+		virtual void Show(void);			   // Shows the values of a circle object, radius, circumfrence, area
+		virtual double Perimeter(void);		   // Calculates a circle objects total perimeter 
+		virtual double Area(void);			   // Calculates a circle objects total area
+		virtual double OverallDimension(void); // Calculates the diameter of the circle
+
 
 };
